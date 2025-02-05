a211990f834a92177cac3eba6ef64d0eb1aee636
Rohith <rohithkumarbathula99@gmail.com>
2025-02-05 22:38:32 +0530

    change edit/ delete icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,10 @@
-import LogoSvg from "../svg/LogoSvg";
+// import LogoSvg from "../svg/LogoSvg";
 import Profile from "./Profile";
 
 const Nav = () => {
   return (
     <div className="m-2 flex justify-around">
-      <LogoSvg />
+      {/* <LogoSvg /> */}
       <h1 className="text-3xl font-bold">Task.io</h1>
       <Profile name="Rohith" />
     </div>
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -38,10 +38,6 @@ const TaskCard = ({
         <p className="justify-between mb-3 font-normal text-gray-700">
           {task.description}
         </p>
-        {/* <p className="font-normal text-gray-700 flex gap-1">
-          {task.status === "completed" ? <GreenFillSvg /> : <RedFillSvg />}
-          <span>{task.status}</span>
-        </p> */}
         <div className="flex justify-end gap-2">
           <button
             onClick={() => {
@@ -52,13 +48,13 @@ const TaskCard = ({
             }}
             className="cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 "
           >
-            Edit <EditSvg />
+            <EditSvg />
           </button>
           <button
             onClick={() => handleDelete(task.id)}
             className="cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300"
           >
-            Delete <DeleteSvg />
+            <DeleteSvg />
           </button>
         </div>
       </div>
